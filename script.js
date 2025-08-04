b6028a44fc5f4efbc3298d2359e7bf5fdc0791cd
kety31 <ketlyn.przywitowski@escola.pr.gov.br>
2025-08-04 12:24:20 -0300

    .

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const caixaPerguntas = document.querySelecto(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativa");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const caixaResultadp = document.querySelector(".texto-resultado");
+const perguntas = [
+    {
+        enunciado:
+        alternativas:
+        
+    }
