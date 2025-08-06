88e47820955c97330d80eddfff29a459beffc71e
kety31 <ketlyn.przywitowski@escola.pr.gov.br>
2025-08-06 09:48:01 -0300

    perguntas.

--- script.js
+++ script.js
@@ -3,9 +3,88 @@ const caixaPerguntas = document.querySelecto(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativa");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const caixaResultadp = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+    enunciado:"Quem foi a primeira modelo mundial?",
+    alternativas:[
+        {
+            texto:"Lisa Fonssagrives",
+            afirmação:"Resposta correta",
+            pontos:1
+        },
+        {
+            texto:"Gisele Bündchen",
+            afirmação:"Resposta errada",
+            pontos:0
+        },
+        {
+            texto:"Adriana Lima",
+            afirmação:"Resposta errada",
+            pontos:0
+        }
+    ]
+},
+]
 const perguntas = [
     {
-        enunciado:
-        alternativas:
-        
-    }
+    enunciado:"Qual é a marca mais famosa no mundo da moda?",
+    alternativas:[
+        {
+            texto:"Adidas",
+            afirmação:"Resposta errada",
+            pontos:0
+        },
+        {
+            texto:"Nike",
+            afirmação:"Resposta correta",
+            pontos:1
+        },
+        {
+            texto:"Louis Vuitton",
+            afirmação:"Resposta errada",
+            pontos:0
+        }
+    ]
+},
+]
+const perguntas = [
+    {
+    enunciado:"Em que ano Gisele bundchen ganhou o premio de modelo do ano no Vogue Fashion?",
+    alternativas:[
+        {
+            texto:"1999",
+            afirmação:"Resposta correta",
+            pontos:1
+        },
+        {
+            texto:"2000",
+            afirmação:"Resposta errada",
+            pontos:0
+        },
+        {
+            texto:"1986",
+            afirmação:"Resposta errada",
+            pontos:0
+        }
+    ]
+},
+]
+const perguntas = [
+    {
+    enunciado:"Quais são os 7 tipos de moda?",
+    alternativas:[
+        {
+            texto:"Sensual, Vulgar, Vibrantes, Clássico, Exagerado, Natural e Vintage",
+            afirmação:"Resposta errada",
+            pontos:0
+        },
+        {
+            texto:"Casual, Elegante, Clássico, Romântico, sexy, Dramático e Criativo",
+            afirmação:"Resposta correta",
+            pontos:1
+        }
+    ]
+},
+]
+    
